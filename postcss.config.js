ba84e7e40d364af64e2b01aaf4afff7f668f6c36
James <m2898ywg2c@privaterelay.appleid.com>
2026-07-10 09:27:55 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-    <title>Sue's Training</title>
-
-    <meta name="description" content="Strength and conditioning tracker" />
-    <meta name="theme-color" content="#2E1A47" />
-
-    <link rel="manifest" href="/manifest.json" />
-
-    <!-- iOS home screen behaviour: opens standalone, no Safari chrome -->
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-    <meta name="apple-mobile-web-app-title" content="Training" />
-    <link rel="apple-touch-icon" href="/icon-192.png" />
-
-    <link rel="icon" type="image/png" href="/icon-192.png" />
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/main.jsx"></script>
-  </body>
-</html>
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
